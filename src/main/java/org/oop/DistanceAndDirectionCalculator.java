325cc01e2891fe52cf993311faef58a72570bd54
punam-shelke <punamshelke.98@gmail.com>
2020-10-18 01:01:45 +0530

    extracted the method of duplicate statements

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -2,14 +2,18 @@ package org.oop;
 
 public class DistanceAndDirectionCalculator {
     public static double distance(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
+        double xDistance = getDistance(to.getX(), from.getX());
+        double yDistance = getDistance(to.getY(), from.getY());
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
     public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
+        double xDistance = getDistance(to.getX(), from.getX());
+        double yDistance = getDistance(to.getY(), from.getY());
         return Math.atan2(yDistance, xDistance);
+    }
+
+    private static double getDistance(double d1, double d2) {
+        return d1 - d2;
     }
 }
