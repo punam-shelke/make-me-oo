334293f35347a72dee2c3b54459168d3894a9ff0
punam-shelke <punamshelke.98@gmail.com>
2020-10-18 02:02:44 +0530

    Extracted duplicated lines into a function getXAndYDistance

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -6,14 +6,12 @@ public class DistanceAndDirectionCalculator {
     private static double yDistance;
 
     public static double distance(Point from, Point to) {
-        xDistance = getDistance(to.getX(), from.getX());
-        yDistance = getDistance(to.getY(), from.getY());
+        getXAndYDistance(from, to);
         return Math.sqrt(square(xDistance) + square(yDistance));
     }
 
     public static double direction(Point from, Point to) {
-        xDistance = getDistance(to.getX(), from.getX());
-        yDistance = getDistance(to.getY(), from.getY());
+        getXAndYDistance(from, to);
         return Math.atan2(yDistance, xDistance);
     }
 
@@ -23,5 +21,10 @@ public class DistanceAndDirectionCalculator {
 
     private static double getDistance(double d1, double d2) {
         return d1 - d2;
+    }
+
+    private static void getXAndYDistance(Point from, Point to) {
+        xDistance = getDistance(to.getX(), from.getX());
+        yDistance = getDistance(to.getY(), from.getY());
     }
 }
