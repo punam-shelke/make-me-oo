6068338eaddcf1e1f03002a62d8f90cd0d9d8d36
punam-shelke <punamshelke.98@gmail.com>
2020-10-18 01:13:29 +0530

    Extracted local variables as field variables

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -1,15 +1,19 @@
 package org.oop;
 
 public class DistanceAndDirectionCalculator {
+
+    private static double xDistance;
+    private static double yDistance;
+
     public static double distance(Point from, Point to) {
-        double xDistance = getDistance(to.getX(), from.getX());
-        double yDistance = getDistance(to.getY(), from.getY());
+        xDistance = getDistance(to.getX(), from.getX());
+        yDistance = getDistance(to.getY(), from.getY());
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
     public static double direction(Point from, Point to) {
-        double xDistance = getDistance(to.getX(), from.getX());
-        double yDistance = getDistance(to.getY(), from.getY());
+        xDistance = getDistance(to.getX(), from.getX());
+        yDistance = getDistance(to.getY(), from.getY());
         return Math.atan2(yDistance, xDistance);
     }
 
