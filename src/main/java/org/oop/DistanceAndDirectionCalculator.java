f098792f5c2b94814632a1f617a5cdbc458bc8fe
punam-shelke <punamshelke.98@gmail.com>
2020-10-18 01:46:58 +0530

    Arranged public methods at top

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -11,14 +11,14 @@ public class DistanceAndDirectionCalculator {
         return Math.sqrt(square(xDistance) + square(yDistance));
     }
 
-    private static double square(double xDistance) {
-        return Math.pow(xDistance, 2);
-    }
-
     public static double direction(Point from, Point to) {
         xDistance = getDistance(to.getX(), from.getX());
         yDistance = getDistance(to.getY(), from.getY());
         return Math.atan2(yDistance, xDistance);
+    }
+
+    private static double square(double xDistance) {
+        return Math.pow(xDistance, 2);
     }
 
     private static double getDistance(double d1, double d2) {
